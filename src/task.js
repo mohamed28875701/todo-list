828f6bbca3941ec76f7bab76a5ca3833868dd29c
mohamed28875701 <mohamedbenhadjlakhal@gmail.com>
2023-02-13 21:29:01 +0100

    Added priority and description

--- src/task.js
+++ src/task.js
@@ -1,10 +1,14 @@
 export default class task{
-    constructor(name,duedate="no date"){
+    constructor(name,duedate="no date",description="",priority="low"){
         this.name=name;
         this.duedate=duedate;
+        this.description=description;
+        this.priority=priority;
     }
     getName(){return this.name}
     setName(name){this.name=name}
     getDueDate(){return this.duedate}
     setDueDate(date){this.duedate=date}
+    setDescription(desc){this.description=desc}
+    setPriority(pr){this.priority=this.priority}
 }
