df7bfed7c973df1479410120f5708c93817f662c
mohamed28875701 <mohamedbenhadjlakhal@gmail.com>
2023-02-13 22:35:54 +0100

    defined the todolist class

---
+++ src/todoList.js
@@ -0,0 +1,33 @@
+import project from "./project";
+import task from "./task";
+export default class todoList{
+    constructor(){
+        this.projects=[];
+        this.projects.push(new project("Home"));
+        this.projects.push(new project("Gym"));
+        this.projects.push(new project("uni"));
+    }
+    setProjects(projects){this.projects=projects}
+    getProjects(){return this.projects}
+    getProject(projectName){
+        return this.projects.find((pr)=> pr.getName()===projectName);
+    }
+    contains(projectName){
+        for(let i =0;i!=this.projects.length;i++){
+            if(this.projects[i].getName()===projectName)
+                return true;
+        }
+        return false;
+    }
+    addProject(proj){
+        if(this.contains(proj.getName()))
+            return ;
+        this.projects.push(proj);
+    }
+    deleteProject(projectName){
+        if(!this.contains(projectName))
+            return ;
+        this.projects=this.projects.filter((p)=> p.getName()!==projectName);
+    }
+    
+}
