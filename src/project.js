a9c315bf71dda07c98b08fe9c276799e07f062b0
mohamed28875701 <mohamedbenhadjlakhal@gmail.com>
2023-02-13 21:55:50 +0100

    defined project class

---
+++ src/project.js
@@ -0,0 +1,30 @@
+import task from "./task";
+export default class project{
+    constructor(name){
+        this.name=name;
+        this.tasks=[];
+    }
+    getName(){return this.name}
+    setName(n){this.name=n;}
+    getTasks(){return this.tasks}
+    setTasks(tasks){this.tasks=tasks;}
+    addTask(task){
+        for(let i=0;i!=this.tasks.length;i++){
+            if(task.getName()===this.tasks[i].getName())
+                return ;
+        }
+        this.tasks.push(task);
+    }
+    contains(taskName){
+        for(let i=0;i!=this.tasks.length;i++){
+            if(taskName===this.tasks[i].getName())
+                return true;
+        }
+        return false;
+    }
+    deleteTask(taskName){
+        if(!this.contains(taskName))
+            return ;
+        this.tasks=this.tasks.filter((t)=> taskName!==t.getName());
+    }
+}
