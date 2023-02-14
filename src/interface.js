a0808ec13804c3cc2d0288fbe4815036e93f6947
mohamed28875701 <mohamedbenhadjlakhal@gmail.com>
2023-02-14 21:27:00 +0100

    added nav side bar

--- src/interface.js
+++ src/interface.js
@@ -1,8 +1,11 @@
 import storage from "./storage";
+import project from "./project";
+import task from "./task";
 
 export default class ui{
-    static loadPage(){
-        ui.header();
+    static load(){
+        ui.header(); 
+        ui.nav();
     }
     static header(){
         const wrapper=document.createElement("div");
@@ -16,7 +19,25 @@ export default class ui{
     }
     static nav(){
         const wrapper=document.createElement("div");
+        wrapper.setAttribute("id","nav");
         const projList=document.createElement("ul");
+        projList.setAttribute("id","projlist");
+        const add=document.createElement("button");
+        ui.loadProject(projList);
+        wrapper.appendChild(projList);
+        const content=document.getElementById("content");
+        content.appendChild(wrapper);
 
+    }
+    static loadProject(list){
+        list.innerHTML="";
+        storage.getTodoList().getProjects().map((proj)=> list.appendChild(ui.createProj(proj.getName())));
+        
+    }
+    static createProj(projName){
+        const li=document.createElement("li");
+        li.textContent=`${projName}`;
+        li.classList.add("proj");
+        return li;
     }
 }
