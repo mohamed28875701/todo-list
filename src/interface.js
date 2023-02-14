b110779c4a577b0d2aa9b125e058888e413437c4
mohamed28875701 <mohamedbenhadjlakhal@gmail.com>
2023-02-14 22:45:05 +0100

    added method to create task ui

--- src/interface.js
+++ src/interface.js
@@ -6,6 +6,8 @@ export default class ui{
     static load(){
         ui.header(); 
         ui.nav();
+        ui.tasks();
+        
     }
     static header(){
         const wrapper=document.createElement("div");
@@ -23,8 +25,11 @@ export default class ui{
         const projList=document.createElement("ul");
         projList.setAttribute("id","projlist");
         const add=document.createElement("button");
+        add.setAttribute("id","add");
+        add.textContent="+";
         ui.loadProject(projList);
         wrapper.appendChild(projList);
+        wrapper.appendChild(add);
         const content=document.getElementById("content");
         content.appendChild(wrapper);
 
@@ -36,8 +41,53 @@ export default class ui{
     }
     static createProj(projName){
         const li=document.createElement("li");
+        li.setAttribute("data-status","inactive");
+        
         li.textContent=`${projName}`;
+        if(li.textContent==="Home"){
+            li.setAttribute("data-status","active");
+        }
+        
         li.classList.add("proj");
         return li;
+    }
+    static tasks(){
+        const wrapper=document.createElement("div");
+        wrapper.setAttribute("id","tasks");
+        const ul=document.createElement("ul");
+        wrapper.appendChild(ul);
+        const content=document.getElementById("content");
+        content.appendChild(wrapper);
+    }
+    static loadTasks(){
+        const active=document.querySelector("[data-status='active']");
+        const tasks=document.getElementById("tasks");
+    }
+    static createTask(task){
+        const li=document.createElement("li");
+        li.setAttribute("id","task");
+        const div=document.createElement("div");
+        const name=document.createElement("p");
+        name.textContent=task.getName();
+        const details=document.createElement("button");
+        const date=document.createElement("p");
+        const del=document.createElement("button");
+        details.setAttribute("id","details");
+        del.setAttribute("id","delete");
+        li.appendChild(name);
+        div.appendChild(details);
+        div.appendChild(date);
+        div.appendChild(del);
+        li.appendChild(div);
+        if(task.getPriority()==="low"){
+            li.style.borderLeftColor="green"
+        }
+        else if(task.getPriority()==="medium"){
+            li.style.borderLeftColor="orange";
+        }
+        else{
+            li.style.borderLeftColor="red";
+        }
+        
     }
 }
